b723447a771f78b91700002f7197eff0bb7d8b04
Gater <sige03830@gmail.com>
2023-06-03 13:03:36 +0900

    pause,result画面

--- Assets/Scripts/GameScene/UIManager.cs
+++ Assets/Scripts/GameScene/UIManager.cs
@@ -7,26 +7,58 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject pausePanel = null!;
     [SerializeField] private GameObject resultPanel = null!;
     [SerializeField] private Text score = null!;
+    private bool pause = false;
+    private bool result = false;
 
-    public void Pause()
+    private void Pause()
     {
         Time.timeScale = 0f;
+        pause = true;
         pausePanel.gameObject.SetActive(true);
     }
 
-    public void Resume()
+    private void Resume()
     {
         pausePanel.gameObject.SetActive(false);
+        pause = false;
         Time.timeScale = 1f;
     }
 
-    public void Continue()
+    private void Continue()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("GameScene");
     }
 
-    public void PushFinish()
+    private void Finish()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("StartScene");
+    }
+
+    public void Result()
+    {
+        Time.timeScale = 0f;
+        result = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            Pause();
+        }
+        if (pause && Input.GetKey(KeyCode.R))
+        {
+            Resume();
+        }
+        if (result && Input.GetKey(KeyCode.C))
+        {
+            Continue();
+        }
+        if ((result || pause)&& Input.GetKey(KeyCode.F))
+        {
+            Finish();
+        }
     }
 }
