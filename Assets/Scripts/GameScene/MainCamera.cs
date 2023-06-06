1315b1d9ba693b94b0ce66f47327db60d0f635ae
Novy <shimizus235813@outlook.jp>
2023-06-06 16:27:16 +0900

    横移動時の機体の角度

--- Assets/Scripts/GameScene/MainCamera.cs
+++ Assets/Scripts/GameScene/MainCamera.cs
@@ -6,14 +6,22 @@ public class MainCamera : MonoBehaviour
     [SerializeField] private float back = 100f;
     [SerializeField] private float up = 200f;
     [SerializeField] private float angle;
+    private GameObject obj;
+    private RocketControl rc;
+    private Vector3 localAngle;
     void Start()
     {
+        obj = GameObject.Find("Rocket");
+        rc = obj.GetComponent<RocketControl>();
         mytransform = this.transform;
         mytransform.localPosition = new Vector3(0, up, back);
-        Vector3 localAngle = mytransform.localEulerAngles;
+        localAngle = mytransform.localEulerAngles;
         localAngle.x = angle; // ローカル座標を基準に、x軸を軸にした回転をangleに変更
         localAngle.y = 0f; // ローカル座標を基準に、y軸を軸にした回転を0に変更
-        localAngle.z = 0f; // ローカル座標を基準に、z軸を軸にした回転を0に変更
+    }
+    void Update()
+    {
+        localAngle.z = -rc.rotationZ; // ローカル座標を基準に、z軸を軸にした回転を0に変更
         mytransform.localEulerAngles = localAngle; // 回転角度を設定
     }
 }
